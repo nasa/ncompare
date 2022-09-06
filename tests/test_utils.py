84dba51d3b9c7611c3e3d5e3a0f643c3ffb69428
danielfromearth <daniel.kaufman@nasa.gov>
2022-09-06 12:17:01 -0400

    rename existing path validator function for clarity

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,23 +2,23 @@ from pathlib import Path
 
 import pytest
 
-from ncompare.utils import coerce_to_str, make_valid_path
+from ncompare.utils import coerce_to_str, ensure_valid_path_exists
 
 
 def test_make_valid_path_with_simple_invalid_str_path():
     with pytest.raises(FileNotFoundError):
-        make_valid_path("whereisthatfile")
+        ensure_valid_path_exists("whereisthatfile")
 
 def test_make_valid_path_with_close_invalid_Path_path():
     with pytest.raises(FileNotFoundError):
-        make_valid_path(Path(__file__).parents[0].resolve() / "thisdoesntexist.py")
+        ensure_valid_path_exists(Path(__file__).parents[0].resolve() / "thisdoesntexist.py")
 
 def test_make_valid_path_from_str_in_repo():
-    returnval = make_valid_path(str(Path(__file__).parents[0].resolve() / "conftest.py"))
+    returnval = ensure_valid_path_exists(str(Path(__file__).parents[0].resolve() / "conftest.py"))
     assert isinstance(returnval, Path)
 
 def test_make_valid_path_from_Path_in_repo():
-    returnval = make_valid_path(Path(__file__).parents[0].resolve() / "conftest.py")
+    returnval = ensure_valid_path_exists(Path(__file__).parents[0].resolve() / "conftest.py")
     assert isinstance(returnval, Path)
 
 def test_coerce_int_to_str():
