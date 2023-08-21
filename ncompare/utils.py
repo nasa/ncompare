6d9e8aa722972abfee14f5a3c49417149c1beaec
Shiv Kokroo <kokroo@users.noreply.github.com>
2023-08-22 01:55:18 +0200

    Simplify ensure_valid_path_exists

--- ncompare/utils.py
+++ ncompare/utils.py
@@ -5,22 +5,13 @@ from typing import Union
 
 def ensure_valid_path_exists(should_be_path: Union[str, Path]) -> Path:
     """Convert input to a pathlib.Path and check that the resulting filepath exists."""
-    fails_to_exist_msg = "Expected file does not exist: "
-    wrong_type_msg = "Unexpected type for something that should be convertable to a Path: "
-
-    if isinstance(should_be_path, str):
-        # Convert to a Path object
+    if isinstance(should_be_path, (str, Path)):
         path_obj = Path(should_be_path)
         if path_obj.exists():
             return path_obj
-        raise FileNotFoundError(fails_to_exist_msg + str(should_be_path))
+        raise FileNotFoundError(f"Expected file does not exist: {path_obj}")
+    raise TypeError(f"Unexpected type for something that should be convertible to a Path: {type(should_be_path)}")
 
-    if isinstance(should_be_path, Path):
-        if should_be_path.exists():
-            return should_be_path
-        raise FileNotFoundError(fails_to_exist_msg + str(should_be_path))
-
-    raise TypeError(wrong_type_msg + str(type(should_be_path)))
 
 def ensure_valid_path_with_suffix(should_be_path: Union[str, Path],
                                   suffix: str = None) -> Path:
