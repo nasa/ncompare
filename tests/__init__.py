82b3e39428d393c8f127456f7fd1e4aa643ba8f1
danielfromearth <daniel.kaufman@nasa.gov>
2024-05-29 11:22:45 -0400

    add copyright header text to top of modules

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,26 @@
-from pathlib import Path
+# Copyright 2024 United States Government as represented by the Administrator of the
+# National Aeronautics and Space Administration. All Rights Reserved.
+#
+# This software calls the following third-party software,
+# which is subject to the terms and conditions of its licensor, as applicable.
+# Users must license their own copies; the links are provided for convenience only.
+#
+# dask - https://github.com/dask/dask/blob/main/LICENSE.txt
+# netCDF4 - https://github.com/Unidata/netcdf4-python/blob/master/LICENSE
+# numpy - https://github.com/numpy/numpy/blob/main/LICENSE.txt
+# xarray - https://github.com/pydata/xarray/blob/main/LICENSE
+# Python Standard Library - https://docs.python.org/3/license.html#psf-license
+#
+# The STITCHEE: STITCH by Extending a dimEnsion platform is licensed under the
+# Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
 
+from pathlib import Path
 
 data_for_tests_dir = Path(__file__).parent.resolve() / "data"
