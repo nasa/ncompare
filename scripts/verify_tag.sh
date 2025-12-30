d28c666d69c7a04f1cc035c0b0a977543e23c815
danielfromearth <daniel.kaufman@nasa.gov>
2025-12-30 11:19:46 -0500

    Create workflow and script for release on publish; use pypi trusted publishing

---
+++ scripts/verify_tag.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+git_tag=$(git describe --tags --abbrev=0)
+current_version=v$(grep -m 1 version pyproject.toml | cut -d'"' -f 2)
+
+echo "${git_tag} ${current_version}"
+
+if [ "$current_version" == "$git_tag" ]; then
+  echo "Version does match git tag"
+else
+  echo "Version does not match git tag! pyproject.toml: ${current_version} vs Tag: ${git_tag} "
+  exit 1
+fi
